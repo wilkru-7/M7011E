2c30b263114c1dec8521bda65784770afd7b5495
André Christofferson <andrechristofferson@hotmail.com>
2021-11-15 16:08:35 +0100

    MongoDB connection added for sigin and signup

--- Hemsida/app.js
+++ Hemsida/app.js
@@ -1,18 +1,29 @@
-const request = require('request');
+var request = require('request');
 var express = require('express');
 var path = require('path');
 var ejs = require('ejs');
+var bodyParser = require('body-parser');
+
 const app = express()
 const port = 3003
 var price, windspeed, consumption;
 
+const { MongoClient } = require("mongodb");
+
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri);
+
+app.use(express.urlencoded({
+    extended: true
+  }))
+
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-    
     request('http://localhost:3002/', { json: true }, (err, res, body) => {
         if (err) { return console.log(err); }
         price = res.body;
-    });
+    }); 
+    
     request('http://localhost:3001/', { json: true }, (err, res, body) => {
         if (err) { return console.log(err); }
         windspeed = res.body;
@@ -23,13 +34,97 @@ app.get('/', (req, res) => {
     });
 
     res.render('index', {price: price, windspeed: windspeed, consumption: consumption})
-    // res.sendFile(path.join(__dirname, '/index.html'));
+})
+app.post('/login', (req,res) => {
+    /* TODO:
+        Check input so no hacking */
+    var username = req.body.username;
+    var password = req.body.password;
+    /* TODO:
+        Catch and no input should return false */
+    var loggedin = login(username, password).catch(console.dir)
+    if(loggedin){
+        res.redirect('/')
+    }
+    //console.log(username)
 })
 
 app.get('/login', (req, res) => {
     res.render('login', {})
 })
+app.get('/register', (req, res) => {
+    res.render('register', {})
+})
+app.post('/register', (req, res) => {
+    var username = req.body.registerUsername;
+    var password = req.body.registerPassword;
+    var password2 = req.body.registerPassword2;
+    console.log(username)
+    console.log(password)
+    console.log(password2)
+
+    var exists = search(username)
+    console.log(exists)
+    if(exists && password==password2){
+        insert(username, password)
+        res.redirect('/login')
+    } else{
+        console.log("Something was entered wrong")
+    }
+    //res.render('register', {})
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
+
+async function insert(_username, _password){
+    try{
+        await client.connect();
+        const database = client.db('M7011E');
+        const users = database.collection('Users');
+        const insert = { username: _username, password: _password };
+        const result =  await users.insertOne(insert);
+        console.log(result)
+    } finally{
+        await client.close();
+    }
+
+}
+
+async function login(_username, _password){
+    try{
+        await client.connect();
+        const database = client.db('M7011E');
+        const users = database.collection('Users');
+        const search = { username: _username};
+        const result =  await users.findOne(search);
+        if(result != null && _password == result.password){
+            console.log("You are logged in")
+            return true
+        } else{
+            console.log("Wrong password (or wrong username)!!!")
+            return false
+        }
+    } finally{
+        await client.close();
+    }
+}
+
+async function search(_username){
+    try{
+        await client.connect();
+        const database = client.db('M7011E');
+        const users = database.collection('Users');
+        const search = {username: _username};
+        const result =  await users.findOne(search);
+        console.log(result)
+        if(result == null){
+            return true
+        }else{
+            return false
+        }
+    } finally{
+        await client.close();
+    }
+}
