dc5aa45993fb4c9b7b8d6f216f93d1c71c352ef0
André Christofferson <andrechristofferson@hotmail.com>
2021-11-16 09:32:10 +0100

    loggedin session

--- Hemsida/app.js
+++ Hemsida/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var ejs = require('ejs');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 const app = express()
 const port = 3003
@@ -17,8 +18,17 @@ app.use(express.urlencoded({
     extended: true
   }))
 
+/* TODO:
+  Change secret to something(?) */
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
+    if(req.session.loggedin){
     request('http://localhost:3002/', { json: true }, (err, res, body) => {
         if (err) { return console.log(err); }
         price = res.body;
@@ -34,6 +44,9 @@ app.get('/', (req, res) => {
     });
 
     res.render('index', {price: price, windspeed: windspeed, consumption: consumption})
+    }else{
+        res.redirect("login")
+    }
 })
 app.post('/login', (req,res) => {
     /* TODO:
@@ -44,6 +57,7 @@ app.post('/login', (req,res) => {
         Catch and no input should return false */
     var loggedin = login(username, password).catch(console.dir)
     if(loggedin){
+        req.session.loggedin = true;
         res.redirect('/')
     }
     //console.log(username)
@@ -81,10 +95,15 @@ app.listen(port, () => {
 async function insert(_username, _password){
     try{
         await client.connect();
+        console.log(1)
         const database = client.db('M7011E');
+        console.log(2)
         const users = database.collection('Users');
+        console.log(3)
         const insert = { username: _username, password: _password };
+        console.log(4)
         const result =  await users.insertOne(insert);
+        console.log(5)
         console.log(result)
     } finally{
         await client.close();
