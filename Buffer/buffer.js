dad990c535dde38e53dddb92261b6a5030dd081e
Wilma <wilkru-7@student.ltu.se>
2021-11-19 09:56:28 +0100

    Added buffer file

---
+++ Buffer/buffer.js
@@ -0,0 +1,19 @@
+const express = require('express')
+
+const app = express()
+const port = 3005
+
+var buffer = 0;
+update();
+
+app.get('/', (req, res) => {
+    res.json(getBuffer());
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+function getBuffer() {
+    return buffer;
+}
