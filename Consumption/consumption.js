dcae050b20e15090dbbf09a1a7d1865c3b8e51ac
André Christofferson <andrechristofferson@hotmail.com>
2021-12-21 16:04:34 +0100

    api for starting and getting consumption

--- Consumption/consumption.js
+++ Consumption/consumption.js
@@ -1,15 +1,41 @@
 const express = require('express')
 var gaussian = require('gaussian');
+/* const Influx = require('influxdb-nodejs');
+
+const client = new Influx('http://127.0.0.1:8086/'); */
+
+const { MongoClient } = require("mongodb");
+const uri = "mongodb://localhost:27017/";
+const client = new MongoClient(uri);
+client.connect();
+const database = client.db('M7011E');
+const users = database.collection('Users');
 
 const app = express()
 const port = 3000
 
 var consumption = 0;
 var consumptionDistribution = gaussian(11, 3);
-update();
 
 app.get('/', (req, res) => {
     res.json(getConsumption());
+})
+
+app.get('/startUser/:user', (req, res) => {
+    var user = req.params['user']
+    var newUser = new Consumer(user)
+    console.log("newUser "+  newUser.username)
+    setInterval(updateConsumption, 1000, newUser)
+    res.json("ok");
+})
+
+app.get('/getUser/:user', (req, res) => {
+    var user = req.params['user']
+    getUserConsumption(user).then(consumption => {
+        console.log("getting: " + consumption)
+        res.send(consumption);
+    })
+
 })
 
 app.listen(port, () => {
@@ -19,8 +45,35 @@ app.listen(port, () => {
 function getConsumption() {
     return consumption;
 }
-function update() {
-    consumption = consumptionDistribution.ppf(Math.random()).toFixed(2);
-};
 
-setInterval(update, 5000);
+class Consumer{
+    constructor(username){
+        this.username = username
+        this.consumption = 0
+        this.consuming = true;
+    }
+}
+
+function updateConsumption(consumer){
+    consumer.consumption = consumptionDistribution.ppf(Math.random()).toFixed(2);
+    insertConsumption(consumer.username, consumer.consumption)
+}
+
+async function insertConsumption(_username, value) {
+    const filter = { username: _username };
+    const options = { upsert: true };
+    const updateDoc = {
+        $set: {
+            consumption: value
+        },
+    };
+    const result = await users.updateOne(filter, updateDoc, options);
+    return result;
+}
+async function getUserConsumption(_username) {
+    const search = { username: _username };
+    var user = await users.findOne(search)
+    if(user.consumption){
+        return user.consumption
+    }
+}
